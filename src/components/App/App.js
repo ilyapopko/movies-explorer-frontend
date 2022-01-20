8ad59d15f2be3c7f162c9c04c231940df720709e
Ilya Popko <ilyap81@gmail.com>
2022-01-20 09:48:25 +0300

    feat: add struct components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Main from '../Main/Main';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import NotFound from '../NotFound/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Movie explorer</h1>
-    </div>
+    // <div className="App">
+    //   <h1>Movie explorer</h1>
+    // </div>
+
+    <>
+      <Switch>
+
+        <Route exact path="/">
+          <Main></Main>
+        </Route>
+
+
+      </Switch>
+    </>
   );
 }
 
